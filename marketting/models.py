7bf7d7aa6d78b6db615fa563b921989111af73a2
hamidafghan <abd.hamidafghan@gmail.com>
2022-03-24 17:59:10 +0100

    django model

--- marketting/models.py
+++ marketting/models.py
@@ -1,3 +1,11 @@
+from pydoc import describe
 from django.db import models
 
 # Create your models here.
+class Note(models.Model):
+    name = models.CharField(max_length=20)
+    decription = models.TextField()
+
+class Card(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
--- marketting/views.py
+++ marketting/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
+from marketting.models import Note
 
 # Create your views here.
 
 
 def welcome(request):
     # get some data from database or an api
+
+    # get all the notes and send it to welcome page
+    notes = Note.objects.all()
+    for note in notes:
+        print(note.name)
+        print(note.id)
+
     return render(request, 'welcome.html', {
         "name": "My todo list application"
     })
