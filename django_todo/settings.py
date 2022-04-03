9bb45132f1ede06ef60cc811e5482bb2cb4729a8
hamidafghan <abd.hamidafghan@gmail.com>
2022-04-03 18:26:35 +0200

    Template

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'marketting'
+    'marketting',
+    'home'
 ]
 
 MIDDLEWARE = [
@@ -127,6 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- django_todo/urls.py
+++ django_todo/urls.py
@@ -17,8 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
+    path('', include('home.urls')),
     path('admin/', admin.site.urls),
     path('marketting/', include('marketting.urls'))
 ]
